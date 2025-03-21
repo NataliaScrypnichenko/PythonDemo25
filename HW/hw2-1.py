b48e4a78571c01470f9b6f3b43ec8c845c314d79
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-03-21 20:51:02 +0200

    готов

--- HW/hw2-1.py
+++ HW/hw2-1.py
@@ -10,3 +10,30 @@
 #
 # #     return ...
 # 2) протипізувати перше завдання
+from typing import Callable
+
+# типізуємо про писані нище ф-ї
+def notebook():  #  -> tuple[Callable[[str], None],[Callable[[],list[str]]]:
+    todo_list: list[str]=[]
+
+    def add_todo(todo:str)->None:
+        nonlocal todo_list # додаємо список в локальне сховище
+        todo_list.append(todo) # додавати список
+
+    def get_all()->list[str]:
+        nonlocal todo_list # додаємо список в локальне сховище
+        return todo_list.copy() #тому що на  todo_list не буде доступу із зовні тільки через ці 2-ї
+
+    return add_todo, get_all
+
+# створюємо 2 списка
+add1, get_all1 = notebook()
+add2, get_all2 = notebook()
+#додаємо
+add1 ('coffe')
+add2 ('appel')
+#виводимо
+print(get_all1())
+print(get_all2())
+
+
--- HW/hw2-2.py
+++ HW/hw2-2.py
@@ -3,8 +3,20 @@
 # (також використовуемо типізацію)
 #
 # Приклад:
-#
 # expanded_form(12) # return '10 + 2'
 # expanded_form(42) # return '40 + 2'
 # expanded_form(70304) # return '70000 + 300 + 4'
 #
+
+def expanded_form(num:int)->str:
+        st=str(num) # перетворюємо число в стрінгу
+        length = len(st)-1 # віднімаються 0 від числа і розбиває його
+        result = [ ] # записуємо результат
+        for i, char in enumerate(st): # розбтраю кожне число масива
+            if char!=0:
+                result.append(char + "0" * (length-i)) # додаємо 0 з кінця
+        return '+'.join(result)+ f' = {st}' # об'єднює результат і додає
+
+print(expanded_form(42))
+print(expanded_form(70304))
+print(expanded_form(12))
