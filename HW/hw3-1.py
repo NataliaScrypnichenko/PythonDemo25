441e5daee7525bf773919b9712800e9180ef3703
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-04-21 20:51:39 +0300

    ok

--- HW/hw3-1.py
+++ HW/hw3-1.py
@@ -1,7 +1,7 @@
 # Створити клас Rectangle:
 # -він має приймати дві сторони x,y
 # -описати поведінку на арифметични методи:
-#   + сумма площин двох екземплярів ксласу
+#   + сумма ПЛОЩИН двох екземплярів ксласу
 #   - різниця площин двох екземплярів ксласу
 #   == площин на рівність
 #   != площин на не рівність
@@ -14,37 +14,44 @@ class Rectangle: # прямоугольник
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def __str__(self):
-        return  str (self. __dict__)
+        self.area = self.x * self.y # потрібно її сюди винисти(я зробила помилку)
+
+    # def __str__(self):
+    #     return  str (self. __dict__)- це не потрібно робити в виведені мат.фігур.
 
     def __add__(self, other: Self): #  + сумма
-        return self.x + other.y
+        return self.area + other.area # Vself.x - other.y=так не потрібно робити моя помилка
 
-    def __sub__(self, other): # - різниця
-       return self.x - other.y
+    def __sub__(self, other: Self): # - різниця
+       return self.area - other.area
 
-    def __eq__(self, other): # == площин
-        return self.x == other.y
+    def __eq__(self, other: Self): # == площин def __eq__(self, other)=не вірно
+        return self.area == other.area
 
-    def __ne__(self, other):
-        return self.x != other.y
+    def __ne__(self, other: Self):
+        return self.area != other.area
 
-    def __lt__(self, other):  # < меньше
-        return self.x < other.y
+    def __lt__(self, other: Self):  # < меньше
+        return self.area < other.area
 
-    def __gt__(self, other):  #  > більше
-        return self.x > other.y
+    def __gt__(self, other: Self):  #  > більше
+        return self.area > other.area
 
-    def __len__(self,other):
-        return self.x + self.y
+    # def __len__(self, other: Self):
+    #     return self.area + self.area=не вірно
 
-r1=Rectangle(4,3)
-r2=Rectangle(8,9)
-# print(r1 + r2) # 13
-# print(r1 - r2) # -5
-# print(r2 < r1) # False
-# print(r1 > r2) # False
-# print(r1 == r2) # False
-# print(r1 != r2) # True
-print(len(r1+r2)) #
+    def __len__(self):  # при виклику метода len() підраховувати сумму сторін
+        return (self.x + self.y) *  2
 
+r1=Rectangle(6,8)
+r2=Rectangle(3,4)
+print(r1+r2)
+print(r1-r2)
+print(r1 == r2)
+print(r1 != r2)
+print(r1 > r2)
+print(r1 < r2)
+print(len(r1))
+print(len(r2))
+
+'''зробила сама не вірно,формули прописала тільки сторони а не врахувала площину,не написала в загальном площину'''
