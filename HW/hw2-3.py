29a459daed331c5f03cd629d5e926fc44ff71abd
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-05-02 15:29:02 +0300

    ok

--- HW/hw2-3.py
+++ HW/hw2-3.py
@@ -22,8 +22,27 @@ func2()
 func2()
 func1()
 func2()
+
+'''def cuont_decor(func):
+    cuont = 0 # скілтки раз викликають лічильник
+
+    def inner(*args, **kwargs): #викликаємо ф
+        nonlocal cuont # передаємо
+        cuont += 1
+        inner(f'{cuont=}') # викликаємо ф
+    return inner # повертаємо
+
+@cuont_decor
+def func1():
+    print('func1')
+
+@cuont_decor
+def func2():
+    print('func2')
+
 func1()
 func2()
 func1()
 func2()
 func1()
+func2()'''
